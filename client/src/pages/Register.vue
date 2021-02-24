6583dcc57318fd25e9f1de7128c6cf5e21c2769a
Toro <gowet92@gmail.com>
2021-02-24 09:25:04 +0100

    Entrega-1Ad Login i registre sense tokenrefresh

--- client/src/pages/Register.vue
+++ client/src/pages/Register.vue
@@ -1,50 +1,50 @@
 <template>
   <q-page class="flex flex-center">
-    <div class="q-pa-md" style="max-width: 400px">
+    <div class="q-pa-md" style="max-width: 500px">
       <q-input
         filled
-        v-model="nom"
-        label="Escriu el teu nom "
-        hint=""
+        v-model="full_name"
+        label="Nom complet "
+        hint="Escriu el teu nom complet"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'escriu algo']"
-      />
-      <q-input
-        filled
-        v-model="cognom"
-        label="Escriu el teus cognoms "
-        hint=""
-        lazy-rules
-        :rules="[ val => val && val.length > 0 || 'escriu algo']"
-      />
-    </div>
-    <div class="q-pa-md" style="max-width: 400px">
-
-      <q-input
-        filled
-        v-model="dni"
-        label="Escriu el teu DNI "
-        hint=""
-        lazy-rules
-        :rules="[ val => val && val.length > 0 || 'escriu algo']"
+        :rules="[ val => val && val.length > 0 || 'No pot estar buit']"
       />
       <q-input
         filled
         v-model="username"
-        label="Escriu el teu usuari "
-        hint=""
+        label="Nom usuari"
+        hint="Escriu el teu nom d'usuari"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'escriu algo']"
+        :rules="[ val => val && val.length > 0 || 'No pot estar buit']"
+      />
+      <q-input
+        filled
+        v-model="dni"
+        label="DNI"
+        hint="Escriu el teu DNI"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'No pot estar buit']"
       />
       <q-input
         filled
         type="password"
         v-model="password"
         label="Contrasenya"
+        hint="Escriu la contrasenya"
+        lazy-rules
+        :rules="[ val => val == password2 ]"
+      />
+      <q-input
+        filled
+        type="password"
+        v-model="password2"
+        label="Contrasenya"
+        hint="Torna escriure la contrasenya"
         lazy-rules
         :rules="[
-          val => val !== null && val !== '' || 'introduir contrasenya'
-        ]"
+         val => val && val.length > 0 || 'Introduir contrasenya',
+         val => val != password || 'han de coincidir les contrasenyes'
+         ]"
       />
 
       <div>
@@ -60,11 +60,11 @@ export default {
   name: 'register',
   data () {
     return {
-      nom: '',
+      full_name: '',
       dni: '',
       username: '',
-      cognom: '',
-      password: ''
+      password: '',
+      password2: ''
 
     }
   }
--- server/index.js
+++ server/index.js
@@ -2,37 +2,77 @@ const express = require("express");
 const https = require("https");
 const fs = require("fs");
 const jwt = require("jsonwebtoken");
-const bodyparser = require("body-parser");
+const bodyParser = require("body-parser");
 let app = express();
 const PORT = 8888;
+const User = require("./models/user");
+
 app.use(bodyParser.json());
 const accessTokenSecret = "toro";
 
 
-https
-  .createServer(
-    {
-      key: fs.readFileSync("app_cert.key"),
-      cert: fs.readFileSync("app_cert.crt"),
-    },
-    app
-  )
-  .listen(PORT, function () {
+
+https.createServer({
+      key: fs.readFileSync("my_cert.key"),
+      cert: fs.readFileSync("my_cert.crt"),
+    },app).listen(PORT, function (){
     console.log("Servidor HTTPS escoltant al port" + PORT + "...");
   });
 
-const authenticateJWT = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (authHeader) {
-    const token = authHeader.split("")[1];
-    jwt.verify(token, accessTokenSecret, (err, user) => {
-      if (err) {
-        return res.sendStatus(403);
-      }
-      req.user = user;
-      next();
-    });
-  } else {
-    res.sendStatus(401);
-  }
-};
+  const authenticateJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+      const token = authHeader.split("")[1];
+      jwt.verify(token, accessTokenSecret, (err, user) => {
+        if (err) {
+          return res.sendStatus(403);
+        }
+        req.user = user;
+        next();
+      });
+    } else {
+      res.sendStatus(401);
+    }
+  };
+
+
+
+app.post('/register',(req,res)=>{
+  var usuario = new User.User
+  usuario.insertUser(
+    req.body.username,
+    req.body.password,
+    req.body.full_name,
+    req.body.dni,
+    req.body.avatar,
+    res,req)
+})
+
+
+app.post("/login",(req,res)=>{
+  var usuario = new User.User
+  usuario.login(
+    req.body.username,
+    req.body.password,
+    req,res
+  )
+})
+
+/*app.post("/login", (req, res) => {
+  var user = new User.User();
+  user.login(req.body.username, req.body.password, (resposta) => {
+    if (resposta) {
+      let autToken = jwt.sign(
+        {
+          username: req.body.username,
+        },
+        accessTokenSecret
+      );
+      res.status(200).json({ autToken });
+    } else {
+      res
+        .status(400)
+        .send({ ok: false, msg: "El usuario o password es incorrecto" });
+    }
+  });
+});*/
